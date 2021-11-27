7455c35eee2b3438527e983d37cc2bfb084f5f89
Seah Bei Ying <beiyingseah@gmail.com>
2021-11-27 20:45:23 +0800

    Added playRound function + refined formatting of choices as input and output

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // Print the choice with the number ID that matches 
 
 
- const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -13,6 +13,35 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     // convert user's input to lowercase (user input is case-insensitive bc conversion will take care of it)
-    let playerSelection = playerSelection.toLowerCase()
+    console.log(computerSelection);
+    // transform user's input to desired format: "Rock", "Paper", "Scissors"
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    // if tie
+    if (computerSelection == playerSelection) {
+        return "It's a tie!";
+    }
+
+    // where computer wins
+    else if (
+        (computerSelection == "Rock" && playerSelection == "Scissors") ||
+        (computerSelection == "Scsissors" && playerSelection == "Paper") ||
+        (computerSelection == "Paper" && playerSelection == "Rock")) 
+    {
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+
+    // where player wins
+    else (
+        (computerSelection == "Rock" && playerSelection == "Paper") ||
+        (computerSelection == "Scissors" && playerSelection == "Rock") ||
+        (computerSelection == "Paper" && playerSelection == "Scissors")) 
+    {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    }
 
   }
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+  
