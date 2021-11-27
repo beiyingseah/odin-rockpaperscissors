a031b0c7e3b23c8dd1f4b391822352cd41d366e8
Seah Bei Ying <beiyingseah@gmail.com>
2021-11-27 23:36:33 +0800

    Added main function, game(), added game title to index.html + updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-rockpaperscissors
+
+In this assignment, I code up a bare-bones web console-mediated game of "Rock Paper Scissors" to put to play what I've learnt in Part 3 of Javascript, particularly functions, if/else conditionals, and working with user inputs.
+In this game, the user plays against the computer via the browser's native "prompt" functionality.
--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // Randomly generate a number
 // Print the choice with the number ID that matches 
 
+// helper functions
 
 const choices = ["Rock", "Paper", "Scissors"];
 
@@ -11,37 +12,56 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    // convert user's input to lowercase (user input is case-insensitive bc conversion will take care of it)
-    console.log(computerSelection);
+function playRound(playerSelection, computerSelection, computerScore, playerScore) {
+    // convert user's input to lowercase (user input can be case-insensitive, conversion will take care of it)
     // transform user's input to desired format: "Rock", "Paper", "Scissors"
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    
+    console.log(`You: ${playerSelection} Computer: ${computerSelection}`);
+
     // if tie
     if (computerSelection == playerSelection) {
-        return "It's a tie!";
-    }
-
-    // where computer wins
+        console.log(`This round is a tie! Computer:${computerScore} You:${playerScore}`);
+    } // where computer wins
     else if (
         (computerSelection == "Rock" && playerSelection == "Scissors") ||
-        (computerSelection == "Scsissors" && playerSelection == "Paper") ||
-        (computerSelection == "Paper" && playerSelection == "Rock")) 
-    {
-        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        (computerSelection == "Scissors" && playerSelection == "Paper") ||
+        (computerSelection == "Paper" && playerSelection == "Rock") 
+    )   {
+        computerScore += 1;
+        console.log(`You lose this round! ${computerSelection} beats ${playerSelection}. Computer:${computerScore} You:${playerScore}`);
+        } // where player wins 
+    else {
+        playerScore += 1;
+        console.log(`You win this round! ${playerSelection} beats ${computerSelection}. Computer:${computerScore} You:${playerScore}`);
     }
 
-    // where player wins
-    else (
-        (computerSelection == "Rock" && playerSelection == "Paper") ||
-        (computerSelection == "Scissors" && playerSelection == "Rock") ||
-        (computerSelection == "Paper" && playerSelection == "Scissors")) 
-    {
-        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    return [computerScore, playerScore];
+}
+
+// main code
+function game() {
+    // initialise scores
+    let computerScore = 0;
+    let playerScore = 0;
+    let results = [computerScore, playerScore];
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose Rock, Paper or Scissors? Type an option and press enter!");
+        results = playRound(playerSelection, computerPlay(), computerScore, playerScore);
+        computerScore = results[0]
+        playerScore = results[1]
+      }
+    final_computerScore = results[0];
+    final_playerScore = results[1];
+    
+    if (final_computerScore == final_playerScore) {
+        return `The game is a tie! Computer:${final_computerScore} You:${final_playerScore}`;
+    } else if (final_computerScore < final_playerScore) {
+        return `You won the game! Computer:${final_computerScore} You:${final_playerScore}`;
+    } else {
+        return `You lost the game! Computer:${final_computerScore} You:${final_playerScore}`;
     }
+}
 
-  }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-  
+
